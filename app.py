f004afa2dd44144c81af9d6ad4647272a9440001
Vivien <vivien.tran-thien_git@m4x.org>
2021-12-31 14:05:52 +0100

    Add English translation

--- app.py
+++ app.py
@@ -10,31 +10,64 @@ from urllib.request import urlretrieve
 url = "https://data.drees.solidarites-sante.gouv.fr/api/records/1.0/download/?dataset=covid-19-resultats-par-age-issus-des-appariements-entre-si-vic-si-dep-et-vac-si"
 url2 = "https://data.drees.solidarites-sante.gouv.fr/explore/dataset/covid-19-resultats-par-age-issus-des-appariements-entre-si-vic-si-dep-et-vac-si/information/"
 
+today = str(datetime.date.today())
+today = f"{today[-2:]}/{today[5:7]}/{today[:4]}"
 
-def transform_status(s):
-    if "Non-vaccinés" in s:
-        return "[0]. Non vaccinés"
-    if "récente" in s:
-        return "[1]. Première dose (≤ 14 jours)"
-    if "efficace" in s:
-        return "[2]. Première dose (> 14 jours)"
-    if "Complet" in s and "sans rappel" in s:
-        return "[3]. Schéma vaccinal complet sans rappel"
-    if "Complet" in s and "avec rappel" in s:
-        return "[4]. Schéma vaccinal complet avec rappel"
+query_params = st.experimental_get_query_params()
+lang2 = "en" if "lang" in query_params and "en" in query_params["lang"] else "fr"
+legend_title = {"fr": "Statut vaccinal", "en": "Vaccine status"}
+xaxis_title = {"fr": "Âge", "en": "Age"}
+yaxis_title = {"fr": "Cas par million de personnes", "en": "Cases per million people"}
+
+lang_selection = st.selectbox("", ["🇫🇷", "🇬🇧"], index=0 if lang2 == "fr" else 1)
+lang = "fr" if lang_selection == "🇫🇷" else "en"
 
 
-def transform_age(s):
-    return {
-        "[20,39]": "20-39 ans",
-        "[40,59]": "40-59 ans",
-        "[60,79]": "60-79 ans",
-        "[80;+]": "80 ans et plus",
-    }[s]
+def transform_status(s, lang):
+    if lang == "fr":
+        if "Non-vaccinés" in s:
+            return "[0]. Non vaccinés"
+        if "récente" in s:
+            return "[1]. Première dose (≤ 14 jours)"
+        if "efficace" in s:
+            return "[2]. Première dose (> 14 jours)"
+        if "Complet" in s and "sans rappel" in s:
+            return "[3]. Schéma vaccinal complet sans rappel"
+        if "Complet" in s and "avec rappel" in s:
+            return "[4]. Schéma vaccinal complet avec rappel"
+    else:
+        if "Non-vaccinés" in s:
+            return "[0]. Not vaccinated"
+        if "récente" in s:
+            return "[1]. First dose (≤ 14 days)"
+        if "efficace" in s:
+            return "[2]. First dose (> 14 jours)"
+        if "Complet" in s and "sans rappel" in s:
+            return "[3]. Fully vaccinated (without booster)"
+        if "Complet" in s and "avec rappel" in s:
+            return "[4]. Fully vaccinated (with booster)"
+
+
+def transform_age(s, lang):
+    if lang == "fr":
+        d = {
+            "[20,39]": "20-39 ans",
+            "[40,59]": "40-59 ans",
+            "[60,79]": "60-79 ans",
+            "[80;+]": "80 ans et plus",
+        }
+    else:
+        d = {
+            "[20,39]": "20-39 years old",
+            "[40,59]": "40-59 years old",
+            "[60,79]": "60-79 years old",
+            "[80;+]": "80+ years old",
+        }
+    return d[s]
 
 
 @st.cache(show_spinner=False)
-def get_and_preprocess_data(today):
+def get_data(today):
     urlretrieve(url, filename="data.csv")
     df = pd.read_csv("data.csv", sep=";", on_bad_lines="skip")
 
@@ -45,9 +78,21 @@ def get_and_preprocess_data(today):
     dates_kept = sorted(list(set(df["date"])))[-15:]
     df = df[df["date"].isin(dates_kept)]
 
+    return df, dates_kept
+
+
+df_initial, dates_kept = get_data(today)
+earliest, latest = dates_kept[0], dates_kept[-1]
+earliest = f"{earliest[-2:]}/{earliest[5:7]}"
+latest = f"{latest[-2:]}/{latest[5:7]}"
+
+
+@st.cache(show_spinner=False)
+def preprocess_data(today, df_initial, lang):
     # Relabel the vaccination status and the age columns
-    df["vac_statut"] = df["vac_statut"].map(transform_status)
-    df["age"] = df["age"].map(transform_age)
+    df = df_initial.copy()
+    df["vac_statut"] = df["vac_statut"].map(lambda s: transform_status(s, lang))
+    df["age"] = df["age"].map(lambda s: transform_age(s, lang))
 
     # Aggregate the data across all dates
     agg_dict = {"hc_pcr": "sum", "sc_pcr": "sum", "dc_pcr": "sum", "effectif": "mean"}
@@ -57,48 +102,25 @@ def get_and_preprocess_data(today):
         .groupby(by=["age", "vac_statut"])
         .agg(agg_dict)
     )
+    df = df.reset_index(level=["age", "vac_statut"])
 
     # Compute the relative numbers of hospital admissions, ICU admissions and deaths
     df["hc_pcr_per_1M"] = df.apply(lambda x: 1e6 * x.hc_pcr / x.effectif, axis=1)
     df["sc_pcr_per_1M"] = df.apply(lambda x: 1e6 * x.sc_pcr / x.effectif, axis=1)
     df["dc_pcr_per_1M"] = df.apply(lambda x: 1e6 * x.dc_pcr / x.effectif, axis=1)
-    df = df.reset_index(level=["age", "vac_statut"]).rename(columns={"age": "Âge"})
-    return df, dates_kept
+
+    return df
 
 
-today = str(datetime.date.today())
-today = f"{today[-2:]}/{today[5:7]}/{today[:4]}"
-df, dates_kept = get_and_preprocess_data(today)
-earliest, latest = dates_kept[0], dates_kept[-1]
-earliest = f"{earliest[-2:]}/{earliest[5:7]}"
-latest = f"{latest[-2:]}/{latest[5:7]}"
+df = preprocess_data(today, df_initial, lang)
 
+sidebar_title = {
+    "fr": "COVID-19 : cas graves en fonction de l'âge et du statut vaccinal en France",
+    "en": "COVID-19: severe cases by age and vaccine status in France",
+}
 
-#@st.cache(hash_funcs={matplotlib.figure.Figure: hash}, show_spinner=False)
-def create_fig(key, title, today):
-    sns.set_context("paper")
-    fig, ax = plt.subplots(figsize=(8, 4))
-    sns.barplot(
-        data=df,
-        x="Âge",
-        y=key,
-        hue="vac_statut",
-        palette=sns.color_palette("rocket"),
-        ax=ax,
-    )
-    handles, labels = ax.get_legend_handles_labels()
-    ax.legend(handles, [x[5:] for x in labels], title="Statut vaccinal")
-    ax.set(
-        ylabel="Cas par million de personnes", title=title,
-    )
-    sns.despine()
-    return fig
-
-
-st.sidebar.header("COVID-19 : cas graves en fonction de l'âge et du statut vaccinal")
-
-st.sidebar.markdown(
-    f"""
+sidebar_text = {
+    "fr": f"""
 Les 3 graphiques de cette page montrent les nombres, par million de personnes, des :
 
 - **hospitalisations**
@@ -108,38 +130,126 @@ Les 3 graphiques de cette page montrent les nombres, par million de personnes, d
 ... **au cours des 15 derniers jours** pour lesquels les données nationales sont disponibles ({earliest}-{latest}) et en fonction de l'**âge** et du **statut vaccinal**.
 
 Ces graphiques sont mis à jour quotidiennement à partir des données de la [DREES]({url2}).
-"""
+""",
+    "en": f"""
+The 3 charts on this page show the relative numbers (per million of people, by **age** and **vaccine status**) of:
+
+- **hospital admissions**
+- **ICU admissions**
+- **deaths**
+
+... **during the last 15 days** for which national data are available ({earliest}-{latest}).
+
+The charts are updated daily with [DREES]({url2}) data.
+""",
+}
+
+titles = {
+    0: {
+        "fr": r"$\bf{Hospitalisations}$"
+        + f" avec test PCR positif du {earliest} au {latest}",
+        "en": r"$\bf{Hospital\ admissions}$"
+        + f" with positive PCR test from {earliest} until {latest}",
+    },
+    1: {
+        "fr": r"$\bf{Entrées\ en\ soins\ critiques}$"
+        + f" avec test PCR positif du {earliest} au {latest}",
+        "en": r"$\bf{ICU\ admissions}$"
+        + f" with positive PCR test from {earliest} until {latest}",
+    },
+    2: {
+        "fr": r"$\bf{Décès}$" + f" avec test PCR positif du {earliest} au {latest}",
+        "en": r"$\bf{Deaths}$"
+        + f" with positive PCR test from {earliest} until {latest}",
+    },
+}
+
+table_title = {
+    "fr": f"<b>Nombre de cas quotidiens avec test PCR positif</b><br>(période du {earliest} au {latest}, 20 ans ou plus)",
+    "en": f"<b>Daily number of cases with positive PCR test</b><br>(period from {earliest} until {latest}, 20 years old or more)",
+}
+
+table_columns = {
+    0: {"fr": "Situation observée", "en": "Actual situation"},
+    1: {
+        "fr": "Situation hypothétique sans vaccins",
+        "en": "Hypothetical situation without vaccines",
+    },
+}
+
+table_rows = {
+    0: {"fr": "Hospitalisations", "en": "Hospital admissions"},
+    1: {"fr": "Entrées en soins critiques", "en": "ICU admissions"},
+    2: {"fr": "Décès", "en": "Deaths"},
+}
+
+table_text = {
+    0: {
+        "fr": "À partir des données précédentes et connaissant l'effectif de chaque classe d'âge, il est possible de comparer la situation réellement observée et une situation hypothétique où chaque personne aurait les mêmes risques que les personnes non vaccinées de sa catégorie d'âge. **Le nombre de cas dans cette situation hypothétique est toutefois sous-estimé car il ne tient pas compte de l'effet de la couverture vaccinale sur la transmission**.",
+        "en": "With these data and the size of the each age group, it's possible to compare the actual situation with a hypothetical situation in which everyone had the same risks as the risks as the non-vaccinated persons of her age group. **The number of cases in this hypothetical situation is however underestimated because it doesn't take into account the impact of vaccination on transmission.**",
+    },
+    1: {
+        "fr": f"Date de mise à jour : {today}, source : [DREES]({url2}), code : [GitHub](https://github.com/vivien000/vaccine-stats)",
+        "en": f"Updated: {today}, source: [DREES]({url2}), code: [GitHub](https://github.com/vivien000/vaccine-stats)",
+    },
+}
+
+# @st.cache(hash_funcs={matplotlib.figure.Figure: hash}, show_spinner=False)
+def create_fig(key, title, lang, today):
+    sns.set_context("paper")
+    fig, ax = plt.subplots(figsize=(8, 4))
+    sns.barplot(
+        data=df,
+        x="age",
+        y=key,
+        hue="vac_statut",
+        palette=sns.color_palette("rocket"),
+        ax=ax,
+    )
+    sns.despine()
+    handles, labels = ax.get_legend_handles_labels()
+    ax.legend(handles, [x[5:] for x in labels], title=legend_title[lang])
+    ax.set(
+        ylabel=yaxis_title[lang], xlabel=xaxis_title[lang], title=title,
+    )
+    return fig
+
+
+st.sidebar.header(sidebar_title[lang])
+st.sidebar.markdown(sidebar_text[lang])
+
+st.markdown(
+    """
+    <style>
+        div.stSelectbox{
+            width: 15% !important;
+        }
+        .element-container {
+            justify-content: flex-end;
+            flex-direction: row;
+        }
+    </style>""",
+    unsafe_allow_html=True,
 )
 
 for key, title in [
-    (
-        "hc_pcr_per_1M",
-        r"$\bf{Hospitalisations}$"
-        + f" avec test PCR positif du {earliest} au {latest}",
-    ),
-    (
-        "sc_pcr_per_1M",
-        r"$\bf{Entrées\ en\ soins\ critiques}$"
-        + f" avec test PCR positif du {earliest} au {latest}",
-    ),
-    (
-        "dc_pcr_per_1M",
-        r"$\bf{Décès}$" + f" avec test PCR positif du {earliest} au {latest}",
-    ),
+    ("hc_pcr_per_1M", titles[0][lang]),
+    ("sc_pcr_per_1M", titles[1][lang]),
+    ("dc_pcr_per_1M", titles[2][lang]),
 ]:
-    st.pyplot(create_fig(key, title, today))
+    st.pyplot(create_fig(key, title, lang, today))
 
 
 @st.cache(show_spinner=False)
 def compute_daily_cases(today):
-    sum_by_vac_status = df.groupby(by=["Âge"]).sum()[
+    sum_by_vac_status = df.groupby(by=["age"]).sum()[
         ["effectif", "hc_pcr", "sc_pcr", "dc_pcr"]
     ]
     age_range_sizes = sum_by_vac_status["effectif"]
     observed = sum_by_vac_status.sum()[["hc_pcr", "sc_pcr", "dc_pcr"]]
     non_vac_rates = (
-        df[df["vac_statut"] == "[0]. Non vaccinés"]
-        .groupby(by=["Âge"])
+        df[df["vac_statut"].map(lambda s: s[1]) == "0"]
+        .groupby(by=["age"])
         .sum()[["hc_pcr_per_1M", "sc_pcr_per_1M", "dc_pcr_per_1M"]]
     )
     counterfactual = {
@@ -157,7 +267,7 @@ observed, counterfactual = compute_daily_cases(today)
 
 st.markdown(
     f"""
-À partir des données précédentes et connaissant l'effectif de chaque classe d'âge, il est possible de comparer la situation réellement observée et une situation hypothétique où chaque personne aurait les mêmes risques que les personnes non vaccinées de sa catégorie d'âge. **Le nombre de cas dans cette situation hypothétique est toutefois sous-estimé car il ne tient pas compte de l'effet de la couverture vaccinale sur la transmission**.
+{table_text[0][lang]}
 
 <style type="text/css">
 table {{margin: auto}}
@@ -168,25 +278,25 @@ td {{width: 33.3%;text-align:center;vertical-align:middle}}
 <tbody>
   <tr style="border: none">
     <td style="border: none"></td>
-    <td colspan="2"><b>Nombre de cas quotidiens avec test PCR positif</b><br>(période du {earliest} au {latest}, 20 ans ou plus)</td>
+    <td colspan="2">{table_title[lang]}</td>
   </tr>
   <tr style="border: none" class="bold">
     <td style="border: none"></td>
-    <td>Situation observée<br></td>
-    <td>Situation hypothétique sans vaccins</td>
+    <td>{table_columns[0][lang]}</td>
+    <td>{table_columns[1][lang]}</td>
   </tr>
   <tr class="background-color">
-    <td class="bold">Hospitalisations</td>
+    <td class="bold">{table_rows[0][lang]}</td>
     <td>{int(observed['hc_pcr']/15)}</td>
     <td>{int(counterfactual['hc_pcr']/15)}</td>
   </tr>
   <tr>
-    <td class="bold">Entrées en soins critiques</td>
+    <td class="bold">{table_rows[1][lang]}</td>
     <td>{int(observed['sc_pcr']/15)}</td>
     <td>{int(counterfactual['sc_pcr']/15)}</td>
   </tr>
   <tr class="background-color">
-    <td class="bold">Décès</td>
+    <td class="bold">{table_rows[2][lang]}</td>
     <td>{int(observed['dc_pcr']/15)}</td>
     <td>{int(counterfactual['dc_pcr']/15)}</td>
   </tr>
@@ -194,7 +304,7 @@ td {{width: 33.3%;text-align:center;vertical-align:middle}}
 </table>
 <br>
 
-Date de mise à jour : {today}, source : [DREES]({url2}), code : [GitHub](https://github.com/vivien000/vaccine-stats)
+{table_text[1][lang]}
 
 """,
     unsafe_allow_html=True,
